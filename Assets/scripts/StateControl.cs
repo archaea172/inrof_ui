9d8446bb5c963baf83cab050e8c718b023309349
aky <akiyugata271@gmail.com>
2025-06-30 17:58:38 +0900

    add service

--- Assets/scripts/StateControl.cs
+++ Assets/scripts/StateControl.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using ROS2;
 
-public class StateControl : MonoBehaviour
+namespace ROS2
 {
-    // Start is called before the first frame update
-    void Start()
+    public class StateControl: MonoBehaviour
     {
-        
-    }
+        private ROS2UnityComponent ros2Unity;
+        private ROS2Node ros2Node;
+        private IClient<lifecycle_msgs.srv.ChangeState_Request, lifecycle_msgs.srv.ChangeState_Response> StateClient;
+        private Task<lifecycle_msgs.srv.ChangeState_Response> asyncTask;
+        // Start is called before the first frame update
+        void Start()
+        {
+            ros2Unity = GetComponent<ROS2UnityComponent>();
+            if (ros2Unity.Ok())
+            {
+                if (ros2Node == null)
+                {
+                    ros2Node = ros2Unity.CreateNode("status_controler");
+                    StateClient = ros2Node.CreateClient<lifecycle_msgs.srv.ChangeState_Request, lifecycle_msgs.srv.ChangeState_Response>(
+                        "joy_vel_converter/change_state"
+                    );
+                }
+            }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        IEnumerator PeriodicAsyncCall(int state)
+        {
+            if (ros2Unity.Ok())
+            {
+                while (!StateClient.IsServiceAvailable())
+                {
+                    yield return new WaitForSecondsRealtime(1);
+                }
+
+                lifecycle_msgs.srv.ChangeState_Request request = new lifecycle_msgs.srv.ChangeState_Request();
+                request.Transition.Id = 1;
+
+                asyncTask = StateClient.CallAsync(request);
+                asyncTask.ContinueWith((task) => { Debug.Log(task.Result.Success); });
+
+                yield return new WaitForSecondsRealtime(1);
+            }
+        }
+
+        public void On_click()
+        {
+            StartCoroutine(PeriodicAsyncCall(1));
+            
+        }
     }
 }

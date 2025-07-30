4e7689b06445a9de06a8df4b715b7a9d3e7ea828
aky <akiyugata271@gmail.com>
2025-07-31 01:20:08 +0900

    add scripts

--- Assets/scripts/UiManagerController.cs
+++ Assets/scripts/UiManagerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class UiManagerController : MonoBehaviour
 {
+    public GameObject NavUi;
+    public GameObject picture;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ShowNavUi();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void ShowNavUi()
+    {
+        NavUi.SetActive(true);
+        picture.SetActive(false);
+    }
+    
+    public void Showpicture()
+    {
+        NavUi.SetActive(false);
+        picture.SetActive(true);
     }
 }
